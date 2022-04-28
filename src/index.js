1a6210f2248feb042ac42f693ac0fb1cb7f99a5d
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com.com>
2022-04-28 10:52:23 +0200

    Add project logic in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
 import _ from 'lodash';
 import './index.css';
+
+
+const todos = [
+  {
+    description: "HackerRank challenges",
+    completed: false,
+    index:1,
+  },
+  {
+    description: "Codechef challenges",
+    completed: false,
+    index:2,
+  },
+  {
+    description: "Codewars challenges",
+    completed: true,
+    index:3,
+  },
+  {
+    description: "Toptal challenges",
+    completed: false,
+    index:4,
+  },
+]
+
+const getTodos = ()=>{
+  const listGroup = document.querySelector(".todo-list-group");
+   todos.map(item => {
+  const listElement = document.createElement('li');
+  listElement.classList = "todo-list todo-item";
+  listElement.innerHTML =`
+        <button type="button" class=${item.completed === true ? "checked-button" : "unchecked-button"}>
+        <i class="fa-solid fa-check"></i></button>
+        <input type="text" class=${item.completed === true ? "decoration" : "undecoration"}  value="${item.description}">
+        <span class="todo-item-more"><i class="fa-solid fa-ellipsis-vertical"></i></span>
+    `
+    listGroup.appendChild(listElement)
+      console.log(listGroup)
+  })
+}
+window.addEventListener('load',getTodos)
