16ff745690b927c99ef9eb08ee9d9b3b8bef529d
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com.com>
2022-04-29 04:04:31 +0200

    Add new js file and move getTodos function inside that

---
+++ src/ModifyTodos.js
@@ -0,0 +1,24 @@
+let todos = JSON.parse(localStorage.getItem('todos')) || [];// eslint-disable-line
+
+const listGroup = document.querySelector('.todo-list-group');
+const newTask = document.querySelector('.todo-add').querySelector('input');
+const getTodos = () => {
+  const todoElement = todos.map((item) => `
+        <li class="todo-list todo-item" id=${item.index}>
+          ${item.completed === true ? `
+            <i class="fa-solid fa-check checked-icon"></i>`
+    : '<i class="fa-solid fa-square unchecked-icon"></i>'}
+          <input type="text" class=${item.completed === true ? 'decoration edit-todo' : ' edit-todo'}  value="${item.description}">
+          <span class="edit-focus-element"></span>
+          <i class="fa-solid fa-trash-can delete-icon"></i>
+          <i class="fa-solid fa-ellipsis-vertical more-icon"></i>
+        </li>`).join('');
+  listGroup.innerHTML = todoElement;
+  return listGroup;
+};
+
+
+
+export {
+ getTodos,
+};
