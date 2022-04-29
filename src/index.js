0623fccc3d3365b0a349bf6b235a6f7dbde3e123
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com.com>
2022-04-29 07:28:15 +0200

    Add eventlistner to the checkbox and remove completed button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';// eslint-disable-line
 import './style.css';
+
 import {
-  addTodos, deleteTodos, editTodos, getTodos,
+  addTodos, deleteTodos, editTodos, getTodos
 } from './ModifyTodos.js';
+import { changeTodoStatus,removeCompletedTodos } from './TodoStatus.js';
 
 const listGroup = document.querySelector('.todo-list-group');
 const newTask = document.querySelector('.todo-add').querySelector('input');
@@ -14,6 +16,8 @@ listGroup.addEventListener('click', (event) => {
   const clickedItem = event.target.classList[event.target.classList.length - 1];
   const li = event.target.parentElement;
   if (clickedItem === 'delete-icon') deleteTodos(li.id);
+  if (clickedItem === 'checked-icon') changeTodoStatus({index:li.id,status:false});
+  if (clickedItem === 'unchecked-icon') changeTodoStatus({index:li.id,status:true});
 });
 
 listGroup.addEventListener('keypress', (event) => {
@@ -21,5 +25,8 @@ listGroup.addEventListener('keypress', (event) => {
   const li = event.target.parentElement;
   if (pressedItem === 'edit-todo') editTodos({ index: li.id, event });
 });
+
+const clearCompleted = document.querySelector(".clear-todo");
+clearCompleted.addEventListener('click', removeCompletedTodos);
 
 window.addEventListener('load', () => { getTodos(); });
