734072932934175f10054fb4f2258735f7f295b3
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com.com>
2022-04-29 07:34:39 +0200

    Add function to remove all completed todos

--- src/TodoStatus.js
+++ src/TodoStatus.js
@@ -6,5 +6,16 @@ const changeTodoStatus = ({index,status})=>{
   localStorage.setItem('todos', JSON.stringify(todos));
   getTodos()
 }
+const removeCompletedTodos = ()=>{
+  const uncompletedTodos = todos.filter(element =>element.completed !== true);
+  console.log(uncompletedTodos)
+  let newTodos = uncompletedTodos.map((element, index) =>{
+    element.index = index +1;
+    return element;
+  })
+  console.log(newTodos)
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  updateUI(newTodos);
+}
 
 export {changeTodoStatus,removeCompletedTodos}
